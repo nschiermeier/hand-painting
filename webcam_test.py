f9b20aff5cef3253e22232977fdbef4f77c51d4b
nschiermeier <nschierm@purdue.edu>
2025-08-18 20:04:27 -0700

    added hand tracking

--- webcam_test.py
+++ webcam_test.py
@@ -34,6 +34,32 @@ def draw_landmarks_on_image(rgb_image, detection_result):
     mp.solutions.drawing_styles.get_default_pose_landmarks_style())
   return annotated_image
 
+def draw_hand_landmarks_on_image(rgb_image, detection_result):
+  pose_landmarks_list = detection_result.hand_landmarks
+  annotated_image = np.copy(rgb_image)
+
+  # Loop through the detected poses to visualize
+  for idx in range(len(pose_landmarks_list)):
+    pose_landmarks = pose_landmarks_list[idx]
+
+  # Draw the pose landmarks
+  pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+  try:
+    pose_landmarks_proto.landmark.extend([
+      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+    ])
+
+    mp.solutions.drawing_utils.draw_landmarks(
+      annotated_image,
+      pose_landmarks_proto,
+      mp.solutions.hands.HAND_CONNECTIONS,
+      mp.solutions.drawing_styles.get_default_pose_landmarks_style())
+  except (UnboundLocalError, ValueError) as e:
+    pass
+  return annotated_image
+
+
+
 # Set up PoseLandmarker object
 model_path = r'C:/Users/Nick/Projects/mirror-the-mask/pose_landmarker.task'
 base_options = python.BaseOptions(model_asset_buffer=open(model_path, 'rb').read()) # Get standard set of options for a posing task
@@ -43,13 +69,22 @@ options = vision.PoseLandmarkerOptions(
   output_segmentation_masks=True) # get an options object using specified base settings previously created
 detector = vision.PoseLandmarker.create_from_options(options) # Get a detector object with our options for this task
 
+hand_path = r'C:/Users/Nick/Projects/mirror-the-mask/hand_landmarker.task'
+base_hand_options = python.BaseOptions(model_asset_buffer=open(hand_path, 'rb').read()) # Open hand path for finger tracking
+
+hand_options = vision.HandLandmarkerOptions(
+  base_options = base_hand_options)
+hand_detector = vision.HandLandmarker.create_from_options(hand_options)
+
+
 for frame in webcam:
     # Show the frames in a cv2 window
     frame_as_img = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
     #cv2.imshow('Webcam Frame', cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
     
     # Determine if a hand is on screen or not
-    """results = mp_hands.process(frame)
+    results = mp_hands.process(frame)
+    """
     if results.multi_hand_landmarks:
       print("Successfully found hand on screen!")
     else:
@@ -60,10 +95,16 @@ for frame in webcam:
     # Detect pose landmarks from current frame
     rgb_frame = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_as_img)
     detection_result = detector.detect(rgb_frame)
+    hand_result = hand_detector.detect(rgb_frame)
 
     # Process the detection result, then display result
     annotated_image = draw_landmarks_on_image(rgb_frame.numpy_view(), detection_result)
+    try:
+      annotated_hand = draw_hand_landmarks_on_image(rgb_frame.numpy_view(),  hand_result)
+    except UnboundLocalError:
+      continue
     cv2.imshow('w', cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+    cv2.imshow('w', cv2.cvtColor(annotated_hand,  cv2.COLOR_RGB2BGR))
 
     # Break the loop if the user presses the 'q' key
     if cv2.waitKey(1) & 0xFF == ord('q'):
